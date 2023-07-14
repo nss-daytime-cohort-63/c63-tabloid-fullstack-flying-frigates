2a0635e7e63a2d4bb239e74b9187d235197edcb3
kawtarzz <kawtaryazzouzi@gmail.com>
2023-07-14 11:02:55 -0500

    initial commit

---
+++ Tabloid/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Tabloid.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
---
+++ Tabloid/Repositories/CategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+
+using Tabloid.Models;
+using Tabloid.Utils;
+
+namespace Tabloid.Repositories
+{
+    public class CategoryRepository : BaseRepository
+    {
+        public CategoryRepository(IConfiguration config) : base(config)
+        {
+            
+        }
+    }
+}
