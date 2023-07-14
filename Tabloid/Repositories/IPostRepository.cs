2a7763c90c0f7ee03d0f70ff883be9ac34681d30
Amanda Asgari-Rad <amanda_asgarirad@yahoo.com>
2023-07-14 14:16:40 -0500

    add, edit, delete methods added

---
+++ Tabloid/Repositories/IPostRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Tabloid.Repositories
+{
+    public interface IPostRepository
+    {
+        void Add(Post post);
+        void Update(Post post);
+        void Delete(int id);
+    }
+
+}
---
+++ Tabloid/Repositories/PostRepository.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using Tabloid.Utils;
+
+namespace Tabloid.Repositories
+{
+    public class PostRepository : BaseRepository, IPostRepository
+    {
+        public PostRepository(IConfiguration configuration) : base(configuration) { }
+
+        public void Add(Post post)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        INSERT INTO Post (Title, Content, ImageLocation, CreateDateTime, PublishDateTime, IsApproved, CategoryId, UserProfileId)
+                        OUTPUT INSERTED.ID
+                        VALUES (@Title, @Content, @ImageLocation, @CreateDateTime, @PublishDateTime, @IsApproved, @CategoryId, @UserProfileId ";
+
+                    DbUtils.AddParameter(cmd, "@Title", post.Title);
+                    DbUtils.AddParameter(cmd, "@Content", post.Content);
+                    DbUtils.AddParameter(cmd, "@ImageLocation", post.ImageLocation);
+                    DbUtils.AddParameter(cmd, "@CreateDateTime", DateTime.Now);
+                    DbUtils.AddParameter(cmd, "@PublishDateTIme", post.PublishDateTime);
+                    DbUtils.AddParameter(cmd, "IsApproved", true);
+                    DbUtils.AddParameter(cmd, "@CategoryId", post.CategoryId);
+                    DbUtils.AddParameter(cmd, "@UserProfileId", post.UserProfileId);
+
+                    post.Id = (int)cmd.ExecuteScalar();
+                }
+            }
+        }
+
+        public void Update(Post post)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        UPDATE Post
+                            SET Title = @Title,
+                                Content = @Content,
+                                ImageLocation = @ImageLocation,
+                                CreateDateTime = @CreateDateTime,
+                                PublishDateTime = @PublishDateTime,
+                                IsApproved = @IsApproved,
+                                CategoryId = @CategoryId,
+                                UserProfileId = @UserProfileId
+                        WHERE Id = @Id";
+
+                    DbUtils.AddParameter(cmd, "@Title", post.Title);
+                    DbUtils.AddParameter(cmd, "@Content", post.Content);
+                    DbUtils.AddParameter(cmd, "@ImageLocation", post.ImageLocation);
+                    DbUtils.AddParameter(cmd, "@CreateDateTime", DateTime.Now);
+                    DbUtils.AddParameter(cmd, "@PublishDateTIme", post.PublishDateTime);
+                    DbUtils.AddParameter(cmd, "IsApproved", true);
+                    DbUtils.AddParameter(cmd, "@CategoryId", post.CategoryId);
+                    DbUtils.AddParameter(cmd, "@UserProfileId", post.UserProfileId);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using(var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"DELETE FROM Post WHERE Id = @Id";
+                    DbUtils.AddParameter(cmd, "@id", id);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        
+    }
+}
